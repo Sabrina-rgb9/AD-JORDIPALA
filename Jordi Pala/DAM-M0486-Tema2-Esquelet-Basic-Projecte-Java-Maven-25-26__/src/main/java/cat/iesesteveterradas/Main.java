e06fa57c07dc9cb9c8e433a5c5ae5385219af2af
Sabrina-rgb9 <selmeftah3.cf@iesesteveterradas.cat>
2025-12-02 19:01:33 +0100

    .Commit sqlite

---
+++ Jordi Pala/DAM-M0486-Tema2-Esquelet-Basic-Projecte-Java-Maven-25-26__/src/main/java/cat/iesesteveterradas/Main.java
@@ -0,0 +1,10 @@
+package cat.iesesteveterradas;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Ejecutando PR210Honor...");
+        PR210Honor.main(args);
+    }
+}
